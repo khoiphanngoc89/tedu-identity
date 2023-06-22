a9c1e7ad92b9b15af3a32adf9b91a537073a16bc
Khoi Phan <khoiphanngoc89@gmail.com>
2023-06-22 20:56:34 +0700

    #96 configure identity server

--- src/Tedu.Identity.IDP/Extensions/HostingExtensions.Config.cs
+++ src/Tedu.Identity.IDP/Extensions/HostingExtensions.Config.cs
@@ -1,9 +1,21 @@
-﻿using Tedu.Infrastructure.Configurations;
+﻿using Microsoft.EntityFrameworkCore;
+using Tedu.Infrastructure.Configurations;
+using Tedu.Infrastructure.Extensions;
 
 namespace Tedu.Identity.IDP.Extensions;
 
 internal static partial class HostingExtensions
 {
+    public static string GetAppConnectionString(this IServiceCollection services)
+    {
+        var dbSettings = services.GetOptions<DatabaseSettings>(nameof(DatabaseSettings));
+        if (dbSettings is null)
+        {
+            throw new ArgumentNullException(nameof(dbSettings));
+        }
+        return dbSettings.ConnectionString;
+    }
+
     private static IServiceCollection AddConfigurationSettings(this IServiceCollection services, IConfiguration configuration)
     {
         var databaseSettings = configuration.GetSection(nameof(DatabaseSettings)).Get<DatabaseSettings>();
@@ -19,6 +31,7 @@ internal static partial class HostingExtensions
 
     private static IServiceCollection ConfigureIdentityServer(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionSrting = services.GetAppConnectionString();
         services.AddIdentityServer(options =>
         {
             // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
@@ -28,11 +41,20 @@ internal static partial class HostingExtensions
             options.Events.RaiseFailureEvents = true;
             options.Events.RaiseSuccessEvents = true;
         })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
-            .AddInMemoryApiResources(Config.ApiResources)
-            .AddTestUsers(TestUsers.Users);
+            .AddDeveloperSigningCredential()
+            //.AddInMemoryIdentityResources(Config.IdentityResources)
+            //.AddInMemoryApiScopes(Config.ApiScopes)
+            //.AddInMemoryClients(Config.Clients)
+            //.AddInMemoryApiResources(Config.ApiResources)
+            //.AddTestUsers(TestUsers.Users)
+            .AddConfigurationStore(opt =>
+            {
+                opt.ConfigureDbContext = c => c.UseSqlServer(connectionSrting, builder => builder.MigrationsAssembly("Tedu.Identity.IDP"));
+            })
+            .AddOperationalStore(opt =>
+            {
+                opt.ConfigureDbContext = c => c.UseSqlServer(connectionSrting, builder => builder.MigrationsAssembly("Tedu.Identity.IDP"));
+            });
         return services;
     }
 
---
+++ src/Tedu.Identity.IDP/Persistence/IdentitySeed.cs
@@ -0,0 +1,32 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Net.WebSockets;
+
+namespace Tedu.Identity.IDP.Persistence;
+
+public static class IdentitySeed
+{
+    public static IHost MigrationDatabase(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        scope.ServiceProvider
+             .GetRequiredService<PersistedGrantDbContext>()
+             .Database
+             .Migrate();
+
+        using var context = scope.ServiceProvider.GetService<ConfigurationDbContext>();
+        try
+        {
+            context.Database.Migrate();
+            if (!context.Clients.Any())
+            {
+                foreach(var client in Config.Clients)
+                {
+
+                }
+            }
+        }
+        catch (Exception ex) { }
+        return host;
+    }
+}
