6a9379161eea5eecaf696f9e486e219d93d8fb8f
Khoi Phan <khoiphanngoc89@gmail.com>
2023-11-27 22:27:13 +0700

    #100 clean code

--- src/Tedu.Identity.Infrastructure/Const/Constants.Swagger.cs
+++ src/Tedu.Identity.Infrastructure/Const/Constants.Swagger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tedu.Identity.Infrastructure.Const;
+﻿namespace Tedu.Identity.Infrastructure.Const;
 
 public static partial class SystemConstants
 {
